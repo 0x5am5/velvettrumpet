f8cb72519b2442b909fd36e6173f9c63e7a0a592
Sam Gregory <samuel@digital-detox.co.uk>
2015-09-03 21:52:24 +0100

    started news feed

--- index.php
+++ index.php
@@ -1,13 +1,68 @@
 <?php
-/*
-Theme Name: Velvet Trumpet Forteen
-Author: Sam Gregory
-Author URI: http://samgregorydigital.co.uk/
-Description: The 2014 theme for Velvet Trumpets WordPress website.
-Version: 1.0
-Text Domain: velvettrumpetforteen
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
 
-*/
+get_header(); ?>
 
-// Silence is golden.
-?>
+	<div id="primary" class="wrapper">
+		<main id="main" class="content" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1><?php single_post_title(); ?></h1>
+				</header>
+			<?php endif; ?>
+
+			<?php
+			// Start the loop.
+			while ( have_posts() ) : the_post(); ?>
+
+				<article>
+				<h2>
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>				
+				</h2>
+				<p>Posted on: <?php the_date(); ?></p>
+					
+					
+				<?php the_content(); ?>
+				</article>	
+
+
+
+			<?php
+			// End the loop.
+			endwhile;
+
+			// Previous/next page navigation.
+			the_posts_pagination( array(
+				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
+				'next_text'          => __( 'Next page', 'twentyfifteen' ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
+			) );
+
+		// If no content, include the "No posts found" template.
+		else :
+
+			get_template_part( 'content', 'none' );
+
+		endif;
+		?>
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
