493942ff768f2d1396670f571c3b33eaa09e63ad
Samuel Gregory <samuel@digital-detox.co.uk>
2016-02-19 19:26:34 +0000

    added new css

--- template-parts/content-productions.php
+++ template-parts/content-productions.php
@@ -21,12 +21,14 @@ foreach(get_pages($args) as $value) :
 		$guid = $value->post_name;
 	?>
 
-		<div class="col-sm-4 production">
-			<a href="<?php echo $guid; ?>">				
-				<?php if($booking) : echo '<span class="icon booking"></span>'; endif; ?>
-				<span class="shade"></span>
-				<img src="<?php echo $production['url']; ?>" alt="<?php echo get_the_title($id).' - '.get_field('production_year', $id) ?>" class="img-responsive">							
-			</a>
+		<div class="col-sm-4">
+			<div class="production">
+				<a href="<?php echo $guid; ?>">				
+					<?php if($booking) : echo '<span class="icon booking"></span>'; endif; ?>
+					<span class="shade"></span>
+					<img src="<?php echo $production['url']; ?>" alt="<?php echo get_the_title($id).' - '.get_field('production_year', $id) ?>" class="img-responsive">							
+				</a>				
+			</div>
 		</div>						
 		
 	<?php endif;
